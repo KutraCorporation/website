44e95a0c01772721fea1d8e542cb2a5d5c5a832c
Mehmet Ali <mehmetalidurusoy73+github@gmail.com>
2026-06-15 17:43:18 +0300

    fix: homepage paragraph to link of sub-brands

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -83,8 +83,8 @@ export default function Home() {
           </div>
         </section>
         <section className="flex flex-row justify-center items-center gap-5 px-3 py-6 md:py-8 lg:py-12 text-center bg-[#111111]" aria-hidden>
-          <p rel="noopener noreferrer" className="text-[#e8e8e8] hover:text-owt1">DeepMatter St.</p>
-          <p rel="noopener noreferrer" className="text-[#e8e8e8] hover:text-owt1">TST Digital</p>
+          <a href="#" rel="noopener noreferrer" className="text-[#e8e8e8] hover:text-owt1">DeepMatter St.</a>
+          <a href="#" rel="noopener noreferrer" className="text-[#e8e8e8] hover:text-owt1">TST Digital</a>
         </section>
         <EcosystemSection products={products.slice(0, 8)} /> 
       </div>
