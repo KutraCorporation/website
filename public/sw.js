911a9b8036b69acc60472d098b0f5601bf0c21a7
Mehmet Ali <153413232+mehmetalidsy@users.noreply.github.com>
2026-06-25 00:31:07 +0300

    fix: removed well-known, service worker and brave goggle file

--- public/sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('install', function () { console.log('Install!'); });
-self.addEventListener("activate", event => { console.log('Activate!'); console.log(event); });
-self.addEventListener('fetch', function (event) { console.log('Fetch!', event.request); });
-
-self.addEventListener('message', (event) => {
-  if (event.data.type === 'GET_VERSION') {
-    event.ports[0].postMessage('1.0');
-  }
-});
